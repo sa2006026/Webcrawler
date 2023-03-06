1f9367e2bd8d3e267e6004bc4932bdd5521e6323
sa2006026 <sa200602687@yahoo.com>
2023-03-06 12:19:47 +0800

    GBA Update

---
+++ Chinadaily.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+with open('chinadaily2.csv', mode='w', encoding='utf-8', newline='') as csv_file:
+    fieldnames = ['Title', 'Link', 'Content']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for i in range(1, 2):
+        url = f"https://www.chinadaily.com.cn/china/governmentandpolicy/page_{i}.html"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        articles = soup.find_all('div', class_="mb10 tw3_01_2")
+        # Extract title and link
+        for article in articles:
+            title_link = article.find('h4').find('a')
+            title = title_link.text.strip()
+            link = title_link['href'].lstrip('/')
+            content_response = requests.get(f"http://{link}")
+            content_soup = BeautifulSoup(content_response.content, 'html.parser')
+            content_div = content_soup.find('div', id='Content')
+            if content_div:
+                paragraphs = content_div.find_all('p')
+                content = []
+                for p in paragraphs:
+                    content.append(p.get_text())
+            else:
+                content = ["Content not found."]
+
+            # Write row to CSV
+            writer.writerow({'Title': title, 'Link': link, 'Content': '\n'.join(content)})
